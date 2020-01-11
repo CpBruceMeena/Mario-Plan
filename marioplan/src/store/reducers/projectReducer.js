aa99662e8926f3d834ada9c2a451ad1068e2466c
CpBruceMeena <sandeep10198@gmail.com>
2020-01-11 11:29:17 +0530

    Displaying the project summary dynamically using connect

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+   projects: [
+     {id: '1', title: "collect all the star", content: "random random random"},
+     {id: "2", title: "help me find peach", content: "random random random"},
+     {id: "3", title: "Yo Boy!!", content: "random random random"}
+   ]
 }
 
 const projectReducer = (state = initState, action) => {
