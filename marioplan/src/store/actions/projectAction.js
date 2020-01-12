49f7827719dcc1624b4247761f4bfd9efbc82333
CpBruceMeena <sandeep10198@gmail.com>
2020-01-12 12:08:07 +0530

    Adding dummy data

--- marioplan/src/store/actions/projectAction.js
+++ marioplan/src/store/actions/projectAction.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
 
-   return (dispatch, getState) => {
-     dispatch({ type: 'CREATE_PROJECT', project });
-    }
+   return (dispatch, getState, { getFirebase, getFirestore }) => {
+     // making async call to database
+     const firestore = getFirestore();
+     firestore.collection('projects').add({
+       ...project,
+       authorFirstName: 'Bruce',
+       authorLastName: 'Meena',
+       authorId: 12345,
+       createdAt: new Date()
+     }).then(() => {
+       dispatch({ type: 'CREATE_PROJECT', project });
+     }).catch((err) => {
+        dispatch({type: 'CREATE_PROJECT_ERROR', err});
+     })
+   }
 };
