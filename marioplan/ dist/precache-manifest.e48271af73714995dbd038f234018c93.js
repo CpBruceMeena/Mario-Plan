ac6b3e16827d77b401494c42264c54bf78600473
CpBruceMeena <sandeep10198@gmail.com>
2020-01-19 15:17:06 +0530

    final step, deploying the app

---
+++ marioplan/ dist/precache-manifest.e48271af73714995dbd038f234018c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942a579c90696ce65db1051a0216d22c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a55392d5da95df6745",
+    "url": "/static/css/main.26158513.chunk.css"
+  },
+  {
+    "revision": "01d2c51d441a7d810f29",
+    "url": "/static/js/2.8bc36c3e.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.8bc36c3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2a55392d5da95df6745",
+    "url": "/static/js/main.70325c0a.chunk.js"
+  },
+  {
+    "revision": "bd1ce89dfa40ec4433c7",
+    "url": "/static/js/runtime-main.fc8183aa.js"
+  }
+]);
